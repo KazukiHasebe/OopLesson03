464db7b32c7037c4bebdb86ed25dec37cc5ae854
KazukiHasebe <31200@ojs.ac.jp>
2020-10-05 13:57:47 +0900

    Chapter4を追加

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -10,7 +10,63 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            
+            var ymCollection = new YearMonth[]
+            {
+                new YearMonth(2018, 6),
+                new YearMonth(2028, 8),
+                new YearMonth(2038, 7),
+                new YearMonth(2048, 9),
+                new YearMonth(2058, 10)
+            };
+
+            Exercise2_2(ymCollection);
+
+            Exercise2_4(ymCollection);
+
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection)
+        {
+            foreach (var ym in ymCollection)
+            {
+                Console.WriteLine(ym);
+            }
+        }
+
+        static YearMonth FindFirst21C(YearMonth[] yms)
+        {
+            foreach (var ym in yms)
+            {
+                if (ym.Is21Century)
+                {
+                    return ym;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection)
+        {
+            var yearmonth = FindFirst21C(ymCollection);
+#if false
+            var s = yearmonth == null ? "21世紀のデータはありません" : yearmonth.ToString();
+            Console.WriteLine(s.Year);
+#else
+            if (yearmonth == null)
+                Console.WriteLine("21世紀のデータはありません");
+            else
+                Console.WriteLine(yearmonth.Year);
+#endif
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection)
+        {
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var ym in array)
+            {
+                Console.WriteLine(ym);
+            }
         }
     }
 }
