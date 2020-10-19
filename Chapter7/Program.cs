150aa10cee8eb6f2f4add87ca52c923c4447c12c
KazukiHasebe <31200@ojs.ac.jp>
2020-10-19 13:35:44 +0900

    第７章演習問題(P202,P203)

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -1,74 +1,60 @@
-﻿using Microsoft.Win32.SafeHandles;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chapter7
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //問題7-1
-            var text = "Cozy lummox gives smart squid who asks for job pen";
-            Exercise1_1(text);
+namespace Chapter7 {
+
+    // List 7-20
+    class Program {
+        static void Main(string[] args) {
+            // コンストラクタ呼び出し
+            var abbrs = new Abbreviations();
+
+            // カウントプロパティの例
+            Console.WriteLine($"{abbrs.Count}\n");
+
+            // Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            // インデクサの利用例
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach (var name in names) {
+                var fullname = abbrs[name];
+                if (fullname == null)
+                    Console.WriteLine("{0}は見つかりません", name);
+                else
+                    Console.WriteLine("{0}={1}", name, fullname);
+            }
             Console.WriteLine();
 
-            Exercise1_2(text);
+            // ToAbbreviationメソッドの利用例
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+                Console.WriteLine("{0} は見つかりません", japanese);
+            else
+                Console.WriteLine("「{0}」の略語は {1} です", japanese, abbreviation);
             Console.WriteLine();
 
-
-        }
-
-        static void Exercise1_1(string text)
-        {
-            var dict = new Dictionary<char, int>();
-            foreach (var item in text.ToUpper())
-            {
-                if ('A' <= item && item <= 'Z')
-                {
-                    if (dict.ContainsKey(item))
-                    {
-                        dict[item] += 1;
-                    }
-                    else
-                    {
-                        dict.Add(item, 1);
-                    }
-                }
+            // FindAllメソッドの利用例
+            foreach (var item in abbrs.FindAll("国際")) {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
             }
+            Console.WriteLine();
 
-            foreach (var item in dict.OrderBy(x => x.Key))
-            {
-                Console.WriteLine($"'{item.Key}' : {item.Value}");
+            // Removeメソッドの例
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine("削除成功");
+            } else {
+                Console.WriteLine("削除失敗");
             }
-        }
+            Console.WriteLine();
 
-        static void Exercise1_2(string text)
-        {
-            var dict = new SortedDictionary<char, int>();
-            foreach (var item in text.ToUpper())
-            {
-                if ('A' <= item && item <= 'Z')
-                {
-                    if (dict.ContainsKey(item))
-                    {
-                        dict[item] += 1;
-                    }
-                    else
-                    {
-                        dict[item] = 1;
-                    }
-                }
-            }
-
-            foreach (var item in dict)
-            {
-                Console.WriteLine($"'{item.Key}' : {item.Value}");
+            foreach (var item in abbrs.Where(x => x.Key.Length == 3)){
+                Console.WriteLine($"{item.Key} = {item.Value}");
             }
         }
     }
